b8521b2684f1c730cfb8b4aeb7d906adcb8548f1
Darina Maier <darina.maier@epfedu.fr>
2025-12-15 17:17:19 +0100

    Création de la classe Grille

---
+++ THREES/src/Grille.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author maier
+ */
+public class Grille { // on crée une grille (matrice) 4x4
+    private int [][] cases; 
+
+    public Grille(int[][] cases) {
+        cases = new int[4][4];
+    }
+}
